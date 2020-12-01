81407a0c358cdf101b59296a4fa4f7577920959b
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-12-01 10:33:48 +0100

    Service 3

--- src/main/java/de/tekup/ex/controllers/CinemaCtrl.java
+++ src/main/java/de/tekup/ex/controllers/CinemaCtrl.java
@@ -1,6 +1,8 @@
 package de.tekup.ex.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
@@ -31,6 +33,14 @@ public class CinemaCtrl {
 	@GetMapping("/studio/{name}/movies")
 	public List<Movie> getMovies(@PathVariable("name") String studioName){
 		return service.getColoredMoviesByStudio(studioName);
+	}
+	
+	@GetMapping("/star/{dateBegin}/{dateEnd}")
+	public Map<Character, Long> getStars(@PathVariable("dateBegin") String dBegin,
+			@PathVariable("dateEnd") String dEnd){
+		LocalDate dateBegin = LocalDate.parse(dBegin);
+		LocalDate dateEnd = LocalDate.parse(dEnd);
+		return service.getNumberMaleAndFemale(dateBegin, dateEnd);
 	}
 	
 	@ExceptionHandler(NoSuchElementException.class)
--- src/main/java/de/tekup/ex/services/CinemaService.java
+++ src/main/java/de/tekup/ex/services/CinemaService.java
@@ -1,10 +1,14 @@
 package de.tekup.ex.services;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -50,6 +54,34 @@ public class CinemaService {
 			}
 		}
 		return coloredMovies;
+	}
+	
+	// Number of Male & Female Star who born in a given period
+	public Map<Character, Long> getNumberMaleAndFemale(LocalDate dateBegin, LocalDate dateEnd) {
+		List<Star> stars = reposStar.findAll();
+		int male=0,female=0;
+		for (Star star : stars) {
+			if(star.getBirthDate().isAfter(dateBegin) && star.getBirthDate().isBefore(dateEnd)) {
+				if (star.getGendre() == 'M') {
+					male++;
+				}else {
+					female++;
+				}
+			}
+		}
+		
+		Map<Character, Integer> map = new HashMap<>();
+		map.put('M', male);
+		map.put('F', female);
+		
+		//return map;
+		
+		return reposStar.findAll()
+		.stream()
+		.filter(star -> star.getBirthDate().isAfter(dateBegin) && star.getBirthDate().isBefore(dateEnd))
+		.collect(Collectors.groupingBy(star -> star.getGendre(), Collectors.counting()));
+		
+		
 	}
 
 }
